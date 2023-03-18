8b7a906902830f772618dfb00f21cb23bf7da841
Thurler <guithurler90@gmail.com>
2023-03-18 16:21:22 -0300

    Added missing implementations for some helper classes in common widget

--- lib/common.dart
+++ lib/common.dart
@@ -6,11 +6,36 @@ abstract class CommonState<T extends StatefulWidget> extends State<T> {
   final Logger logger = Logger();
   late SaveFile saveFile;
 
-  List<U> buildSeparatedList<U>(List<U> base, U separator) {
-    List<U> result = <U>[separator];
-    for (U element in base) {
-      result.addAll(<U>[element, separator]);
+  List<U> buildSeparatedList<U>(
+    List<U> base,
+    U separator, {
+    bool separateEnds = false,
+  }) {
+    List<U> result = <U>[];
+    if (separateEnds) {
+      result.add(separator);
+    }
+    for (int i = 0; i < base.length - 1; i++) {
+      result.addAll(<U>[base[i], separator]);
+    }
+    result.add(base[base.length - 1]);
+    if (separateEnds) {
+      result.add(separator);
     }
     return result;
+  }
+
+  Row makeRowFromWidgets(List<Widget> widgets, {bool expanded = false}) {
+    if (expanded) {
+      widgets = widgets.map((Widget w)=>Expanded(child: w)).toList();
+    } else {
+      widgets = widgets.map((Widget w)=>Flexible(child: w)).toList();
+    }
+    return Row(
+      children: buildSeparatedList(
+        widgets,
+        const SizedBox(width: 20),
+      ),
+    );
   }
 }
