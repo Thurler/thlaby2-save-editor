7c7a4631c1bc0afa8192d10c888b9e614ef391ed
Thurler <guithurler90@gmail.com>
2023-04-02 12:33:25 -0300

    Added useful extra information for subclass class

--- lib/save/character.dart
+++ lib/save/character.dart
@@ -68,30 +68,35 @@ enum CharacterName {
 }
 
 enum Subclass {
-  none,
-  guardian,
-  monk,
-  warrior,
-  sorcerer,
-  healer,
-  enhancer,
-  hexer,
-  toxicologist,
-  magician,
-  herbalist,
-  strategist,
-  gambler,
-  diva,
-  transcendent,
-  swordmaster,
-  archmage,
-  appraiser,
-  elementalist,
-  ninja,
-  oracle,
-  holyblessing,
-  dragongodpower,
-  winner,
+  none('None', isUnique: false),
+  guardian('Guardian', isUnique: false),
+  monk('Monk', isUnique: false),
+  warrior('Warrior', isUnique: false),
+  sorcerer('Sorcerer', isUnique: false),
+  healer('Healer', isUnique: false),
+  enhancer('Enhancer', isUnique: false),
+  hexer('Hexer', isUnique: false),
+  toxicologist('Toxicologist', isUnique: false),
+  magician('Magician', isUnique: false),
+  herbalist('Herbalist', isUnique: false),
+  strategist('Strategist', isUnique: false),
+  gambler('Gambler', isUnique: false),
+  diva('Diva', isUnique: false),
+  transcendent('Transcendent', isUnique: false),
+  swordmaster('Swordmaster', isUnique: false),
+  archmage('Archmage', isUnique: false),
+  appraiser('Appraiser', isUnique: false),
+  elementalist('Elementalist', isUnique: false),
+  ninja('Ninja', isUnique: false),
+  oracle('Oracle', isUnique: false),
+  holyblessing('Holy Blessing', isUnique: true),
+  dragongodpower("Dragon God's Power", isUnique: true),
+  winner('*WINNER*', isUnique: true);
+
+  final String prettyName;
+  final bool isUnique;
+
+  const Subclass(this.prettyName, {required this.isUnique});
 }
 
 class CharacterData {
